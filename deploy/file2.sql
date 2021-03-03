cac05c838cbdd65dce281d23cc403b1a34f63349
Ruben Louis Jean <60328963+rubenslj00@users.noreply.github.com>
2021-03-03 13:17:06 -0500

    Update file2.sql

--- deploy/file2.sql
+++ deploy/file2.sql
@@ -4,18 +4,12 @@
 USE WAREHOUSE &warehouse;
 
 -- XXX Add DDLs here.
-CREATE TABLE demo.employee(
+CREATE TABLE jenkins_schema(
     empid   INT        PRIMARY KEY AUTOINCREMENT,
     name  TEXT         NOT NULL,
     address  TEXT         NOT NULL,
     emailid TEXT         NOT NULL
 );
-Insert into demo.employee(name,address,emailid) values
-('Rutuja Gholap','Pune','rutuja.gholap@snowflake.com'),
-('Ankita Pawar','Nashik','ankita.pawar@snowflake.com'),
-('Komal Jadhav','Mumbai','komal.jadhav@snowflake.com'),
-('Gargi Kadam','Nagpur','gargi.kadam@snowflake.com'),
-('Oshan Sharma','Jammu','oshan.sharma@snowflake.com');
 
 
-CREATE TABLE DEMO.WEATHER(CITY VARCHAR(15),DAY VARCHAR(10),MAX_TEMP FLOAT,MIN_TEMP FLOAT);
+CREATE TABLE jenkins_schema.WEATHER(CITY VARCHAR(15),DAY VARCHAR(10),MAX_TEMP FLOAT,MIN_TEMP FLOAT);
